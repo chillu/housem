37e4a257ce8ca3047d12c53fba43573cace85600
Ingo Schommer <me@chillu.com>
2021-10-05 11:39:30 +1300

    Basic Hasura metadata/migration setup

---
+++ hasura/migrations/default/1633387112922_init/up.sql
@@ -0,0 +1,33 @@
+SET check_function_bodies = false;
+CREATE TABLE public.activities (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    title text NOT NULL,
+    area_id uuid NOT NULL
+);
+CREATE TABLE public.areas (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    title text NOT NULL
+);
+CREATE TABLE public.my_activities (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    activity_id uuid NOT NULL,
+    user_id text NOT NULL
+);
+CREATE TABLE public.users (
+    id text NOT NULL,
+    name text NOT NULL
+);
+ALTER TABLE ONLY public.activities
+    ADD CONSTRAINT activity_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.areas
+    ADD CONSTRAINT area_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.my_activities
+    ADD CONSTRAINT my_activities_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.activities
+    ADD CONSTRAINT activity_area_id_fkey FOREIGN KEY (area_id) REFERENCES public.areas(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE ONLY public.my_activities
+    ADD CONSTRAINT my_activities_activity_id_fkey FOREIGN KEY (activity_id) REFERENCES public.activities(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE ONLY public.my_activities
+    ADD CONSTRAINT my_activities_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
